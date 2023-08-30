6a40111d9690d65222df31765564375cd7840040
martinromario55 <martinromario55@gmail.com>
2023-08-30 11:15:48 +0300

    taskmate - Initial commit

---
+++ taskmate/src/App.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react'
+
+import { Header } from './components/Header'
+import { AddTask } from './components/AddTask'
+import { ShowTask } from './components/ShowTask'
+
+import './App.css'
+
+function App() {
+  const [tasklist, setTasklist] = useState(
+    JSON.parse(localStorage.getItem('tasklist')) || []
+  )
+
+  // Store content in the local storage
+  useEffect(() => {
+    localStorage.setItem("tasklist", JSON.stringify(tasklist))
+  })
+
+  const [task, setTask] = useState({})
+
+  return (
+    <div className="App">
+      <Header />
+      <AddTask
+        tasklist={tasklist}
+        setTasklist={setTasklist}
+        task={task}
+        setTask={setTask}
+      />
+      <ShowTask
+        tasklist={tasklist}
+        setTasklist={setTasklist}
+        task={task}
+        setTask={setTask}
+      />
+    </div>
+  )
+}
+
+export default App
